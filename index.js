b69784e3c93b4f8a41b25c16ba5a62b06c8db558
cwpeng <padalab@gmail.com>
2019-01-28 15:03:21 +0800

    Add MySQL

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+let mysql=require("mysql");
+let con=mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"123456"
+});
+con.connect(function(err){
+	if(err){
+		throw err;
+	}else{
+		console.log("Connected!");
+	}
+});
 let express=require("express");
 let app=express();
 app.listen(80, function(){
 	console.log("Server Started");
 });
+// git password: af7258ba52ea0bd3756239234f5f46812cc57510 
